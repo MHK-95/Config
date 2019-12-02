e4c215ea37c62dae78a0d9d5be9829f954d81d77
anvar.szulczyk@gmail.com <anvar.szulczyk@gmail.com>
2019-12-01 19:27:34 -0500

    More changes

--- create_symlinks.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# vimrc
-rm ~/.vimrc 2> /dev/null
-ln -s ~/config/dotfiles/vimrc ~/.vimrc
-
-# bashrc
-rm ~/.bashrc 2> /dev/null
-ln -s ~/config/dotfiles/bashrc ~/.bashrc
---
+++ setup_configs.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# vimrc
+rm ~/.vimrc 2> /dev/null
+ln -s ~/config/dotfiles/vimrc ~/.vimrc
+
+# bashrc
+rm ~/.bashrc 2> /dev/null
+ln -s ~/config/dotfiles/bashrc ~/.bashrc
+
+# Disable Animations
+gsettings set org.gnome.desktop.interface enable-animations false
+
+# Setup my Dash with my favorite apps
+gsettings set org.gnome.shell favorite-apps "['firefox.desktop', 'gnome-terminal.desktop', \
+    'org.gnome.Nautilus.desktop']"
+
+# Terminal
+
+gsettings set org.gnome.Terminal.Legacy.Settings confirm-close false
+
+# KeyBindings for Teminal
+gsettings_schema=org.gnome.Terminal.Legacy.Keybindings
+gsettings_path=/org/gnome/terminal/legacy/keybindings/
+schema_path=$gsettings_schema:$gsettings_path
+
+gsettings set $schema_path zoom-in '<Ctrl>j'
+gsettings set $schema_path zoom-out '<Ctrl>k'
+
+
